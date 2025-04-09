c9a7e23abbddf1a48d8e7123dc7ab888b7eedbbd
Hayko <hayk.ghukasyannnn@gmail.com>
2025-04-09 21:18:08 +0400

    Add files via upload

---
+++ handlers.py
@@ -0,0 +1,16 @@
+from aiogram import Router
+from aiogram.filters import CommandStart
+from aiogram.types import Message, FSInputFile
+import app.keyboards as kb
+
+router = Router()
+
+@router.message(CommandStart())
+async def cmd_start(message: Message):
+    photo = FSInputFile("Flux_Dev_The_image_depicts_a_spacethemed_scene_with_a_view_of__2.jpg")  # պատկերի տեղադրությունը
+    await message.answer_photo(
+    photo, 
+    caption=f" Hey👋 @{message.from_user.username} Welcome to the HyperVerse", 
+    reply_markup=kb.main
+)
+
---
+++ run.py
@@ -0,0 +1,23 @@
+import asyncio
+import logging
+
+import asyncio
+import logging
+
+from handlers import router
+from aiogram import Bot, Dispatcher
+from config import TOKEN
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+async def main():
+    dp.include_router(router)
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Exit')
